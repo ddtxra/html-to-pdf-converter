5bee57a460b3c05ca3b5bd562f8ab27f0f288ab9
DINIS TEIXEIRA Daniel <Daniel.DinisTeixeira@hcuge.ch>
2020-11-12 18:24:09 +0100

    adding server

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const request = require('request');
 const express = require('express');
 var path = require('path');
 
-var render_url =  "http://renderer:3000/convert/url"
+//var render_url =  "http://renderer:3000/convert/url"
+var render_url =  "http://vmlp-01026:8899/convert/url"
+
 
 // Constants
 const PORT = 8855;
@@ -14,37 +16,43 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
-function setDefaultValueForParams(query) {
-  if(!query.paperWidth) query.paperWidth = '13.33'
-  if(!query.paperHeight) query.paperHeight = '16'
-  if(!query.paperWidth) query.paperWidth = '13.33'
-  if(!query.marginTop) query.marginTop = '0'
-  if(!query.marginBottom) query.marginBottom = '0'
-  if(!query.marginLeft) query.marginLeft = '0'
-  if(!query.marginRight) query.marginRight = '0'
-  if(!query.waitTimeout) query.waitTimeout = '30'
-  return query;
-}
+function getOptions(params, url) {
+    var options = {}
+    options.remoteURL = url;
+    if (!params.paperWidth) options.paperWidth = '13.33'; else options.paperWidth = params.paperWidth;
+    if (!params.paperHeight) options.paperHeight = '16';  else options.paperHeight = params.paperHeight;
+    if (!params.paperWidth) options.paperWidth = '13.33'; else options.paperWidth = params.paperWidth;
+    if (!params.marginTop) options.marginTop = '0'; else options.marginTop = params.marginTop;
+    if (!params.marginBottom) options.marginBottom = '0'; else options.marginBottom = params.marginBottom;
+    if (!params.marginLeft) options.marginLeft = '0'; else options.marginLeft = params.marginLeft;
+    if (!params.marginRight) options.marginRight = '0'; else options.marginRight = params.marginRight;
+    if (!params.waitTimeout) options.waitTimeout = '30'; else options.waitTimeout = params.waitTimeout;
 
+    return options;
+}
+  
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/pdf', (req, res) => {
-  //res.setHeader('Content-disposition', 'inline; filename="print.pdf"');
-  res.setHeader('Content-type', 'application/pdf');
-  var params = setDefaultValueForParams(req.query);
-  console.log("Calling rendering url with " + JSON.stringify(params));
-  var filename = __dirname + "/document.pdf";
-  var stream = request.post({url:render_url, formData: params}).pipe(fs.createWriteStream(filename))
-  stream.on('finish', () =>{
-        fs.readFile(filename, function (err,data){
-            res.contentType("application/pdf");
-            res.send(data);
-        })
-  });
-});
 
+app.get('/pdf/**', (req, res) => {
+    //res.setHeader('Content-disposition', 'inline; filename="print.pdf"');
+    res.setHeader('Content-type', 'application/pdf');
+    var url = req.originalUrl.substr("/render-pdf/".length);
+    var params = getOptions(req.query, url);
+    console.log("Calling rendering url with " + JSON.stringify(params));
+    var filename = __dirname + "/document.pdf";
+    var stream = request.post({url:render_url, formData: params}).pipe(fs.createWriteStream(filename))
+    stream.on('finish', () =>{
+          fs.readFile(filename, function (err,data){
+              res.contentType("application/pdf");
+              console.log("Sending data");
+              res.send(data);
+          })
+    });
+});
+  
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
