de5039293d2a91da2293a589324c59836050145c
Daniel Teixeira <ddtxra@gmail.com>
2020-11-12 01:38:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Convenient web service to transform HTML on PDF.
+The web service uses Gotenberg renderer https://github.com/thecodingmachine/gotenberg
+
+Instructions
+```
+docker-compose up -d
+```
+
+Access url example
+```
+http://localhost:8855/render?remoteURL=https://facebook.com&marginTop=20
+```
---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('fs');
+const request = require('request');
+const express = require('express');
+var path = require('path');
+
+var render_url =  "http://renderer:3000/convert/url"
+
+// Constants
+const PORT = 8855;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+function setDefaultValueForParams(query) {
+  if(!query.paperWidth) query.paperWidth = '13.33'
+  if(!query.paperHeight) query.paperHeight = '16'
+  if(!query.paperWidth) query.paperWidth = '13.33'
+  if(!query.marginTop) query.marginTop = '0'
+  if(!query.marginBottom) query.marginBottom = '0'
+  if(!query.marginLeft) query.marginLeft = '0'
+  if(!query.marginRight) query.marginRight = '0'
+  return query;
+}
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/render', (req, res) => {
+  //res.setHeader('Content-disposition', 'inline; filename="print.pdf"');
+  res.setHeader('Content-type', 'application/pdf');
+  var params = setDefaultValueForParams(req.query);
+  console.log("Calling rendering url with " + JSON.stringify(params));
+  request.post({url:render_url, formData: params}).pipe(res)
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
